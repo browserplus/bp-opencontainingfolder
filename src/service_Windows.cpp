076972448b5e9d2af66339c5a6b3339b0a308b41
Gordon Durand <gordon_durand@yahoo.com>
2010-06-16 14:15:11 -0700

    Use ShellExecuteW() rather than SHOpenFolderAndSelectItems().  The latter always immediately closes the window on the first invocation.

--- src/service_Windows.cpp
+++ src/service_Windows.cpp
@@ -20,6 +20,7 @@
  * ***** END LICENSE BLOCK ***** */
 
 #include <shlobj.h>
+#include <ShellAPI.h>
 #include "service.h"
 #include "bpservice/bpservice.h"
 #include "bpservice/bpcallback.h"
@@ -38,23 +39,53 @@ OpenContainingFolder::doOpen(const bpf::Path& path,
         errMsg = "CoInitialize failed";
         return false;
     }
-
     bool rval = true;
+
+#ifdef NOTDEF
     bpf::tString full = path.external_file_string();
     ITEMIDLIST* pidl = ::ILCreateFromPath(full.c_str());
     hr = ::SHOpenFolderAndSelectItems(pidl, 0, 0, 0);
     if (FAILED(hr)) {
+        // Yeesh.  For some reason, shell windows will sometimes 
+        // immediately close.  Thus, we open it again.  If it closed,
+        // this will open it.  If it didn't close, it's a no-op.
+        // I love windows.
+        log(BP_WARN, "SHOpenFolderAndSelectItems failed, trying again: " + ss.str());
+        hr = ::SHOpenFolderAndSelectItems(pidl, 0, 0, 0);
+        if (FAILED(hr)) {
+            errMsg = string("Unable to open folder for ") + path.externalUtf8();
+            rval = false;
+        }
+    }
+
+    ::ILFree(pidl);
+#else
+    bpf::tString params(L"/select,");
+    params += path.external_file_string();
+    int h = (int) ::ShellExecuteW(0, L"open", L"C:\\WINDOWS\\explorer.exe",
+                                  params.c_str(), NULL,
+                                  SW_SHOWNORMAL);
+    if (h <= 32) {
+        LPTSTR msg;
+        DWORD dw = ::GetLastError();
+        DWORD res = ::FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+                                    FORMAT_MESSAGE_FROM_SYSTEM |
+                                    FORMAT_MESSAGE_IGNORE_INSERTS,
+                                    NULL, dw,
+                                    MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                                    (LPTSTR)&msg, 0, NULL );
+        stringstream ss;
+        ss << "(" << dw << ")";
+        if (res) {
+            bpf::tString s(msg);
+            ss << ": " << bpf::utf8FromNative(s);
+            LocalFree(msg);
+        }
+        log(BP_ERROR, ss.str());
         errMsg = string("Unable to open folder for ") + path.externalUtf8();
         rval = false;
     }
-
-    // Yeesh.  For some reason, shell windows will sometimes 
-    // immediately close.  Thus, we open it again.  If it closed,
-    // this will open it.  If it didn't close, it's a no-op.
-    // I love windows.
-    (void) ::SHOpenFolderAndSelectItems(pidl, 0, 0, 0);
-
-    ::ILFree(pidl);
+#endif
     ::CoUninitialize();
     return rval;
 }
